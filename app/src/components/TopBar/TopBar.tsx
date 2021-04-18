c1f5b9a967d6c33f225f65c5c83f6aa77308e443
Vebjørn Kvisli <vkvisli@gmail.com>
2021-04-18 13:49:16 +0200

    Add favicon and top bar logo

--- app/src/components/TopBar/TopBar.tsx
+++ app/src/components/TopBar/TopBar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './TopBar.css';
+import logo from "../../assets/images/bus.png";
 
 class TopBar extends React.Component {
   render() {
     return (
       <div className="top-bar">
-        Timetable - Jernbanetorget Bus Stop
+        <img className="top-bar-logo" src={logo} alt="Bus logo" width="48" height="48"/>
+        <span className="top-bar-header">Timetable - Jernbanetorget Bus Stop</span>
       </div>
     );
   }
