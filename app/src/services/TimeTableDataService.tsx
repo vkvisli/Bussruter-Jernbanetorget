f6c3f7ca46eaf13a566df9a415f466a46f807bf9
Vebjørn Kvisli <vkvisli@gmail.com>
2021-04-18 18:35:24 +0200

    Add fetching of timetable data

--- app/src/services/TimeTableDataService.tsx
+++ app/src/services/TimeTableDataService.tsx
@@ -1,8 +1,39 @@
-class TimeTableDataService {
-    public static getTimeTableData(): any {
-        console.log("getting time table data...");
-        return null;
+import ITimeTable from "../interfaces/ITimeTable";
 
+class TimeTableDataService {
+    private apiUrl: string = "https://api.entur.io/journey-planner/v2/graphql";
+
+    public getTimeTableData(): Promise<ITimeTable> {
+        return new Promise<ITimeTable>((resolve, reject) => {
+            
+            fetch(this.apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "ET-Client-Name": "test"
+                },
+                body: JSON.stringify({
+                    query: `{
+                        stopPlace(id: "NSR:StopPlace:4000") {
+                            id
+                            name
+                            estimatedCalls(startTime:"${new Date().toISOString()}" timeRange: 72100, numberOfDepartures: 10) {
+                                realtime
+                                aimedArrivalTime
+                                expectedArrivalTime
+                                date
+                                destinationDisplay {
+                                    frontText
+                                }
+                            }
+                        }
+                    }`
+                })
+            })
+            .then(res => res.json())
+            .then(res => { resolve(res.data) });
+
+        });
     }
 }
 
